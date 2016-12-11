191f2c9e900e5f461d5eabefd03ccaaf646d6934
evdel <etienne.delmotte@efrei.net>
2016-12-11 21:09:35 +0100

    Supperssion des premiers graphiques qui servaient d'entraînements basiques. Taille augmentée pour le diagramme en barres.

--- display_graphs.js
+++ display_graphs.js
@@ -1,43 +1,11 @@
-var data = [4, 23, 15, 16, 23, 42];
-
-//
-
-d3.select("#barChart")
-.selectAll("div")
-.data(data)
-.enter().append("div")
-.text(function(d){return d;})
-.classed("bar", true)
-.style("width","500px")
-.style("width", function(d){return 7 * d + "px";});
-
-chart = d3.select("#svgBarChart");
-chart.attr("width","500px")
-.attr("height", 30*data.length + "px");
-
-var bar = chart.selectAll("g")
-.data(data).enter().append("g")
-.attr("transform", function(d, i){return "translate(0,"+i*20 + ")";});
-
-bar.append("rect")
-.attr("width", function(d){return 7*d;})
-.attr("height","19");
-
-bar.append("text")
-.attr("x",function(d){return d-3;})
-.attr("y","9,5")
-.attr("dy", ".35em")
-.text(function(d){return d;});
-
-
 // Barres verticales
 
 data = [4, 23, 15, 16, 42, 18];
 
 // - paramétrage
 
-var chartHeight = 200;
-var chartWidth = 400;
+var chartHeight = 400;
+var chartWidth = 350;
 var yAxisThickness = 40;
 var xAxisThickness = yAxisThickness;
 
